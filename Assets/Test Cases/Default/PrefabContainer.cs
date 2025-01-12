d41eba1bb4f6350577f4ec52569e57a570ce4083
Ulaş TOSUN <40580957+UlasTosun@users.noreply.github.com>
2025-01-12 23:06:15 +0300

    Default case added

---
+++ Assets/Test Cases/Default/PrefabContainer.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class PrefabContainer : MonoBehaviour {
+
+    [SerializeField] private List<GameObject> _prefabs = new();
+
+    private GameObject _loadedPrefab;
+
+
+
+    public void LoadRandomPrefab() {
+        if (_prefabs.Count == 0) {
+            Debug.LogError("No prefabs to load");
+            return;
+        }
+
+        if (_loadedPrefab != null)
+            Destroy(_loadedPrefab);
+
+        _loadedPrefab = Instantiate(_prefabs[Random.Range(0, _prefabs.Count)], transform);
+    }
+
+
+
+}
